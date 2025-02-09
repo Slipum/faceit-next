8eb4df89e35884889887eac7771facaf4bcc997d
Slipum <sabirovdanil7@gmail.com>
2025-02-09 13:13:24 +0300

    fix

--- app/comp/page.tsx
+++ app/comp/page.tsx
@@ -1,28 +1,27 @@
 'use client';
 
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import Compare from './Compare';
 import ComparisonStatistic from './ComparisonStatistic';
 
-export default function CompPage({
-	searchParams,
-}: {
-	searchParams: { player?: string; with?: string };
-}) {
+export default function CompPage() {
+	const searchParams = useSearchParams();
+	const player = searchParams?.get('player') || '';
+	const compareWith = searchParams?.get('with') || '';
+
 	const [first, setFirst] = useState<string>('');
 	const [second, setSecond] = useState<string>('');
 
 	return (
 		<>
-			<Link
-				href={`/profile?search=${searchParams.player}`}
-				className="undo-main">
+			<Link href={`/profile?search=${player}`} className="undo-main">
 				<i className="fa-solid fa-reply fa-2xl"></i>
 			</Link>
 			<ComparisonStatistic
-				player={searchParams?.player || ''}
-				compareWith={searchParams?.with || ''}
+				player={player}
+				compareWith={compareWith}
 				setFirst={setFirst}
 				setSecond={setSecond}
 			/>
